2be69ea8d54c7f7e29d7f809fae8688c877fe338
Марк Пономаренко <ponomarenko.marke@gmail.com>
2023-04-24 11:36:07 +0300

    fourth solution upload

---
+++ hw2/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Text;
+
+namespace hw2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.Unicode;
+            var speeds = CarProcessor.GenerateCarData();
+            var maxSpeeds = CarProcessor.FindMaxSpeedCar(speeds);
+            Console.WriteLine($"Швидкість [{string.Join(", ", speeds)}]");
+            Console.WriteLine($"Індекс першого максимального: {maxSpeeds.Item1}, останнього: {maxSpeeds.Item2}");
+        }
+    }
+}
