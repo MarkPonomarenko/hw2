5b9ce35dcb71045ba5bea8647e70343f7061d987
Марк Пономаренко <ponomarenko.marke@gmail.com>
2023-04-24 11:32:24 +0300

    third solution upload

---
+++ hw2/BookProcessor.cs
@@ -0,0 +1,16 @@
+﻿namespace hw2
+{
+    public class BookProcessor
+    {
+        public static int[] GeneratePageData()
+        {
+            Random rnd = new Random();
+            return Enumerable.Range(0, 100).Select(_ => rnd.Next(10, 1000)).ToArray();
+        }
+
+        public static int FindBookWMaxPages(int[] pages) 
+        {
+            return pages.Max();
+        }
+    }
+}
---
+++ hw2/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Text;
+
+namespace hw2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.Unicode;
+            var pages = BookProcessor.GeneratePageData();
+            Console.WriteLine($"Сторінки [ {string.Join(", ", pages)} ]");
+            var maxPages = BookProcessor.FindBookWMaxPages(pages);
+            Console.WriteLine($"Сторінок в найтовшій книжці: {maxPages}");
+        }
+    }
+}
