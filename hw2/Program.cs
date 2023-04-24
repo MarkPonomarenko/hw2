e3f5926a9cc904146c9164344f7aac55b1a527e5
Марк Пономаренко <ponomarenko.marke@gmail.com>
2023-04-24 11:29:13 +0300

    second solution upload

---
+++ hw2/Program.cs
@@ -0,0 +1,44 @@
+﻿using System.Text;
+
+namespace hw2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.Unicode;
+            StringParser parser = new StringParser();
+            Console.WriteLine("Введіть текст:");
+            string input;
+            do
+            {
+                input = Console.ReadLine();
+            } while (string.IsNullOrWhiteSpace(input));
+            Console.WriteLine("Виділити цифри чи числа? (1/0)");
+            ConsoleKey[] readKeys = { ConsoleKey.Escape, ConsoleKey.D0, ConsoleKey.D1 };
+            ConsoleKey option;
+            do
+            {
+                option = Console.ReadKey(true).Key;
+            } while (!readKeys.Contains(option));
+            string trimmedString = parser.GetTrimmedString(input);
+            int maxIndex = -1;
+            bool subject = false;
+            switch (option)
+            {
+                case ConsoleKey.D0:
+                    maxIndex = parser.GetMaxNumberIndex(trimmedString);
+                    subject = false;
+                    break;
+                case ConsoleKey.D1:
+                    maxIndex = parser.GetMaxDigitIndex(trimmedString);
+                    subject = true;
+                    break;
+                case ConsoleKey.Escape:
+                    return;
+            }
+            Console.WriteLine($"Текст без відступів з початку: {trimmedString}");
+            Console.WriteLine($"Індекс макс {(subject ? "цифри" : "числа")}: {maxIndex}");
+        }
+    }
+}
---
+++ hw2/StringParser.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hw2
+{
+    public class StringParser
+    {
+        public string GetTrimmedString(string input)
+        {
+            return input.TrimStart();
+        }
+
+        public int GetMaxDigitIndex(string input)
+        {
+            var digits = input.Where(char.IsDigit).Select(x => x - '0');
+            return !digits.Any() ? -1 : input.IndexOf((char)(digits.Max() + 48));
+        }
+
+        public int GetMaxNumberIndex(string input)
+        {
+            StringBuilder integerStr = new StringBuilder();
+            StringBuilder stringBuilder = new StringBuilder();
+            int count = 0;
+            foreach (char c in input)
+            {
+                if (char.IsDigit(c))
+                {
+                    integerStr.Append(c);
+                }
+                else if (integerStr.Length > 0)
+                {
+                    stringBuilder.Append(integerStr);
+                    stringBuilder.Append('.');
+                    integerStr.Clear();
+                }
+            }
+            if (integerStr.Length > 0)
+            {
+                stringBuilder.Append(integerStr);
+                stringBuilder.Append('.');
+            }
+            string[] numbersStr = stringBuilder.ToString().Split(".");
+            int[] numbers = new int[numbersStr.Length - 1];
+            for (int i = 0; i < numbersStr.Length - 1; i++)
+            {
+                numbers[i] = int.Parse(numbersStr[i]);
+            }
+            return !numbers.Any() ? -1 : input.IndexOf(numbers.Max().ToString());
+        }
+    }
+}
